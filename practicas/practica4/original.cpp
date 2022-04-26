580cc22765ffc350c892251df91ad94fd2549b69
p72losaj <55488505+p72losaj@users.noreply.github.com>
2022-04-26 08:54:29 +0200

    Add files via upload

---
+++ practicas/practica4/original.cpp
@@ -0,0 +1,10 @@
+/*
+	Original algorithm practice 4
+	Autor: Jaime Lorenzo Sanchez
+*/
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+using namespace std;
+int A[4224][4224], B[4224][4224], C[4224][4224], D[4224][4224]; 
