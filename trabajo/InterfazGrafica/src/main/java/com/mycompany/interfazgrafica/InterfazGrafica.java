5b53a8421ed890d799f39368a1161ce3d64e34df
p72losaj <p72losaj@uco.es>
2022-04-27 10:52:39 +0200

    Inicio interfaz grafica

---
+++ trabajo/InterfazGrafica/src/main/java/com/mycompany/interfazgrafica/InterfazGrafica.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.interfazgrafica;
+
+/**
+ *
+ * @author jaime
+ */
+public class InterfazGrafica {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {             
+              public void run() {                 
+                  new Inicio().setVisible(true);             
+              }
+      });
+    }
+}
